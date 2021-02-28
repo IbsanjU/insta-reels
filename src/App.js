ea2f8d4dc1c26f3edc3954b9f41e418e8beff9f3
IbsanjU <bharath2668@gmail.com>
2021-02-28 15:41:59 +0530

    Host in firebase
    https://ibs-react.web.app/

--- src/App.js
+++ src/App.js
@@ -16,14 +16,6 @@ function App() {
 
   return (
     <div className="app">
-      {/* <div className="app__top">
-
-        {/* {image at top - logo} * /}
-      <img className="app__logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/100px-Instagram_logo_2016.svg.png" alt="" />
-
-      {/* {Reels text } * /}
-        <h1>Insta - Reels</h1>
-      </div> */}
 
       <div className="app__videos">
         {/* {container of app__videos (scrollable container)} */}
@@ -66,4 +58,6 @@ export default App;
 
 // firebase login
 // firebase init
+// npm run build
 // firebase deploy
+
